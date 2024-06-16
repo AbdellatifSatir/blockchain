ae232de49e0bffcb32d0596be0dc2fb0d6e21737
AbdellatifSatir <abdo.satir0811@gmail.com>
2024-06-16 12:49:00 +0100

    4rd

--- block.py
+++ block.py
@@ -3,15 +3,15 @@ from hashlib import sha256
 class Block():
 
     def __init__(self, id, data, timestamp, prev):
-        self.id = id
-        self.data = data
-        self.timestamp = timestamp
-        self.prev = prev
+        self.id = str(id)
+        self.data = str(data)
+        self.timestamp = str(timestamp)
+        self.prev = str(prev)
         self.hash = self.generate_hash()
 
     def display(self):
-        return str(self.id) + str(self.data) + str(self.timestamp) + str(self.prev )
+        return self.id + self.data + self.timestamp + self.prev
 
     def generate_hash(self):
-        data = str(self.id) + str(self.data) + str(self.timestamp) + str(self.prev )
+        data = self.id + self.data + self.timestamp + self.prev
         return sha256(data.encode("UTF-8")).hexdigest()
